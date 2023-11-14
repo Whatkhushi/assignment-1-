1504794313724dfecb9fc0850b8148db6be0c2ea
Whatkhushi <145923136+Whatkhushi@users.noreply.github.com>
2023-11-14 22:27:18 +0530

    Update week 1.cpp

--- week 1.cpp
+++ week 1.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+
+void findMinMax(int arr[], int size, int &min, int &max) {
+    min = max = arr[0];
+
+    for (int i = 1; i < size; ++i) {
+        if (arr[i] < min) {
+            min = arr[i]; 
+        } else if (arr[i] > max) {
+            max = arr[i]; 
+        }
+    }
+}
+
+int main() {
+    const int size = 5; 
+    int arr[size];
+
+    std::cout << "Enter " << size << " elements of the array:\n";
+    for (int i = 0; i < size; ++i) {
+        std::cin >> arr[i];
+    }
+
+    int min, max;
+    findMinMax(arr, size, min, max);
+
+    std::cout << "Minimum element: " << min << std::endl;
+    std::cout << "Maximum element: " << max << std::endl;
+
+    return 0;
+}
 
